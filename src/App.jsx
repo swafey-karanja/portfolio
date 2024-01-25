04cd13ead8caffca04f40a8b0657b62caf4dd53c
swafey karanja <kswafey@gmail.com>
2024-01-25 15:55:28 +0300

    day 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,17 @@ import Testimonials from "./components/testimonials/Testimonials";
 import Topbar from "./components/tobbar/Topbar";
 import Work from "./components/work/Work";
 import "./app.scss";
+import { useState } from "react";
+import Menu from "./components/menu/Menu";
 
 
 function App() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
-      <Topbar/>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro/>
         <Portfolio/>
--- src/components/tobbar/Topbar.jsx
+++ src/components/tobbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import "./topbar.scss"
 import { Person, Mail } from "@mui/icons-material";
 
-export default function Topbar() {
+export default function Topbar( { menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">Kswafey</a>
@@ -16,7 +16,13 @@ export default function Topbar() {
             <span>Kswafey@gmail.com</span>
           </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   )
